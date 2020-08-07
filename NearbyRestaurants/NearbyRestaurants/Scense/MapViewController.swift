276aa2458683ee1e732f8d87dd38c82f475ace9b
Thanathip <thanathip.kum@truedigital.com>
2020-08-08 00:03:14 +0700

    [modify][success] add camera zoom

--- NearbyRestaurants/NearbyRestaurants/Scense/MapViewController.swift
+++ NearbyRestaurants/NearbyRestaurants/Scense/MapViewController.swift
@@ -19,13 +19,14 @@ final class MapViewController: UIViewController, CLLocationManagerDelegate {
         return manager
     }()
     private var coordinateRegion: MKCoordinateRegion?
+    private var markerArray: [GMSMarker] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
         self.setupMapView()
         self.searchTextField.delegate = self
     }
-
+    
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(true)
         
@@ -72,10 +73,60 @@ final class MapViewController: UIViewController, CLLocationManagerDelegate {
             }
             
             for item in response.mapItems {
+                guard let location = item.placemark.location else {
+                    return
+                }
                 print(item.name ?? "not available")
                 print(item.phoneNumber ?? "there aren't phone number.")
+                self.addMarker(location: location.coordinate, title: item.name ?? "")
             }
+            self.setCameraZoomBound() 
         })
+    }
+    
+    private func searchRestaurant(textToSearch: String) {
+        guard let region = self.coordinateRegion else {
+            return
+        }
+        let request = MKLocalSearch.Request()
+        request.naturalLanguageQuery = "\(textToSearch)"
+        request.region = region
+        let search = MKLocalSearch(request: request)
+        search.start(completionHandler: { (response, error) in
+            guard let response = response else {
+                debugPrint(error?.localizedDescription ?? "unknow error")
+                return
+            }
+            self.gmsMapView.clear()
+            self.markerArray.removeAll()
+            for item in response.mapItems {
+                guard let location = item.placemark.location else {
+                    return
+                }
+                print(item.name ?? "not available")
+                print(item.phoneNumber ?? "there aren't phone number.")
+                self.addMarker(location: location.coordinate, title: item.name ?? "")
+            }
+            self.setCameraZoomBound()
+        })
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        switch status {
+        case .restricted:
+            print("Location access was restricted.")
+        case .denied:
+            print("User denied access to location.")
+            // Display the map using the default location.
+            gmsMapView.isHidden = true
+        case .notDetermined:
+            print("Location status not determined.")
+        case .authorizedAlways: fallthrough
+        case .authorizedWhenInUse:
+            print("Location status is OK.")
+        @unknown default:
+            fatalError()
+        }
     }
     
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
@@ -103,11 +154,29 @@ final class MapViewController: UIViewController, CLLocationManagerDelegate {
         alertController.addAction(okAction)
         self.present(alertController, animated: true, completion: nil)
     }
+    
+    private func addMarker(location: CLLocationCoordinate2D, title: String) {
+        let marker = GMSMarker()
+        marker.position = location
+        marker.title = title
+        marker.map = self.gmsMapView
+        self.markerArray.append(marker)
+    }
+    
+    private func setCameraZoomBound() {
+        var bounds = GMSCoordinateBounds()
+        for marker in markerArray {
+            bounds = bounds.includingCoordinate(marker.position)
+        }
+        let update = GMSCameraUpdate.fit(bounds, withPadding: 60)
+        self.gmsMapView.animate(with: update)
+    }
 }
 
 extension MapViewController: UITextFieldDelegate {
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
         self.searchTextField.endEditing(true)
+        self.searchRestaurant(textToSearch: textField.text ?? "")
         return true
     }
 }
