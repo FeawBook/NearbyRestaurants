88d2a66c741009ade50dc6f07b2689ff7b599d6c
Thanathip <thanathip.kum@truedigital.com>
2020-08-07 13:19:22 +0700

    [modify][success] init project

---
+++ NearbyRestaurants/NearbyRestaurants/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NearbyRestaurants
+//
+//  Created by Thanathip Kumnarai on 7/8/2563 BE.
+//  Copyright © 2563 xx. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
